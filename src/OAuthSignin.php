2f57721d7a55129729bb44cb95b3bcab34c9d33b
Richard <solleer@hotmail.com>
2018-08-12 14:11:55 -0400

    Fix user signin

--- src/OAuthSignin.php
+++ src/OAuthSignin.php
@@ -4,31 +4,38 @@ use Solleer\User\{SigninStatus};
 
 class OAuthSignin {
     private $model;
-    private $user;
+    private $config;
     private $userStatus;
     private $oauthStatus;
 
-    public function __construct(Authentication $model, User $user, SigninStatus $userStatus, OAuthStatus $oauthStatus) {
+    public function __construct(Authentication $model, Config $config, SigninStatus $userStatus, OAuthStatus $oauthStatus) {
         $this->model = $model;
-        $this->user = $user;
+        $this->config = $config;
         $this->userStatus = $userStatus;
         $this->oauthStatus = $oauthStatus;
     }
 
     public function signin($code) {
         $accessToken = $this->model->getAccessToken($code);
-        $user = (object) $this->user->getUser();
+        if (!$accessToken) return false;
         $this->oauthStatus->setOAuthVars([
             'access_token' => $accessToken->getToken(),
             'refresh_token' => $accessToken->getRefreshToken(),
             'token_expires' => $accessToken->getExpires()
         ]);
+        $clientFactory = new ClientFactory($this->oauthStatus->getToken(), $this->config);
+        $userModel = new User($clientFactory->getClient());
+        $user = (object) $userModel->getUser();
         $this->userStatus->setSigninID($user->id);
-        else return false;
+        return true;
+    }
+
+    public function getProvider() {
+        return $this->model->getProvider();
     }
 
     public function signout() {
-        $this->oauthStatus->setOAuthVars(null);
+        $this->oauthStatus->setOAuthVars([null]);
         $this->userStatus->setSigninID(null);
         return true;
     }
