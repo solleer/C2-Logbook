c108d01d9c61c5bf70024209d01a3ece4c451882
Richard <solleer@hotmail.com>
2018-08-12 13:58:18 -0400

    Add caching for API calls
    Reduces load times for pages from minutes to seconds

--- src/User.php
+++ src/User.php
@@ -7,8 +7,9 @@ class User {
     private $client;
     private $id;
     private $addedResultFields = ['num_intervals'];
+    private $cache = [];
 
-    public function __construct(Client $client, $id = "me") {
+    public function __construct(Client $client, string $id = "me") {
         $this->client = $client;
         $this->id = $id;
     }
@@ -27,24 +28,48 @@ class User {
      * The filter parameter accepts 4 properties: from, to, type, updated_after
      */
     public function getResults($filter) {
-        $response = $this->client->request('GET', $this->id . '/results', [
-            'query' => $filter]);
-        $data = $this->interpretResponse($response);
-        if (!$data) return false;
+        $filter = array_intersect_key($filter, ['from' => 0, 'to' => 0, 'type' => 0, 'updated_after' => 0]);
+        $cacheId = md5(serialize($filter));
 
-        foreach ($this->getOtherResultPages($this->decodeBody($response->getBody())) as $pageResult)
-            $data = array_merge($data, $pageResult);
-        foreach ($data as $key => $workout) $data[$key] = $this->addAdditionalResultFields($workout);
-        return $data;
+        if (!isset($this->cache[$cacheId])) {
+            $filter['number'] = 200;
+            $filter['page'] = 1;
+
+            $response = $this->client->request('GET', $this->id . '/results', [
+                'query' => $filter]);
+            $data = $this->interpretResponse($response);
+
+            if (!$data) return false;
+
+            foreach ($this->asyncGetOtherPages($this->decodeBody($response->getBody()), $filter) as $pageResult)
+                $data = array_merge($data, $pageResult);
+            foreach ($data as $key => $workout) $data[$key] = $this->addAdditionalResultFields($workout);
+
+            $this->cache[$cacheId] = $data;
+        }
+
+        return $this->cache[$cacheId];
     }
 
-    private function getOtherResultPages($response) {
-        while (isset($response['meta']['pagination']['links']['next'])) {
-            $nextPageLink = $response['meta']['pagination']['links']['next'];
+    private function asyncGetOtherPages($response, $filter) {
+        if (!isset($response['meta']['pagination'])) return [];
 
-            $response = $this->client->request('GET', $nextPageLink]);
-            $data = $this->interpretResponse($response);
-            if ($data) yield $data;
+        // Initiate each request but do not block
+        $promises = [];
+        for ($i = 2; $i <= $response['meta']['pagination']['total_pages']; $i++) {
+            $filter['page'] = $i;
+            $promises[] = $this->client->getAsync($this->id . '/results', ['query' => $filter]);
+        }
+
+        // Wait on all of the requests to complete. Throws a ConnectException
+        // if any of the requests fail
+        $results = \GuzzleHttp\Promise\unwrap($promises);
+
+        // Wait for the requests to complete, even if some of them fail
+        $results = \GuzzleHttp\Promise\settle($promises)->wait();
+
+        foreach ($results as $value) {
+            yield $this->interpretResponse($value['value']);
         }
     }
 
@@ -86,14 +111,14 @@ class User {
     }
 
     private function removeAddedResultFields(\stdClass $data): \stdClass  {
-        foreach ($this->addedFields as $field) unset($data->$field);
+        foreach ($this->addedResultFields as $field) unset($data->$field);
         return $data;
     }
 
     private function addAdditionalResultFields(array $data): array {
         if (isset($data['workout_type'])) { // Ensure its a workout
-            if (strpos($data['workout_type'], "Interval") !== false)
-                $data['num_intervals'] = count($data['workout']) ?? null;
+            if (strpos($data['workout_type'], "Interval") !== false && isset($data['workout']))
+                $data['num_intervals'] = count($data['workout']['intervals']) ?? null;
         }
         return $data;
     }
